f8289a4b862eeb5bc3ad702c1867dde9476d6b36
Bannikov Andrey <andrey27x777@gmail.com>
2019-09-25 22:22:13 +0300

    Included card switcher by Vue.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,34 @@
 var app = new Vue({
   el: "#app",
   data: {
-    message: "Страница 3 из 7"
+    byRows: false
+  },
+  computed: {
+    containerClasses: function() {
+      return {
+        "p-0": this.byRows,
+        "pb-37": !this.byRows,
+        "pl-27": !this.byRows,
+        "pt-md-4": !this.byRows,
+        "pr-md-19": !this.byRows,
+        "pb-md-14": !this.byRows,
+        "pl-md-23": !this.byRows,
+        "shadow-sm": !this.byRows,
+        "section-container--light_bg": !this.byRows
+      };
+    },
+    cardModifClass: function() {
+      return {
+        "card--row": this.byRows
+      };
+    }
+  },
+  methods: {
+    displayBlocks: function() {
+      return (this.byRows = false);
+    },
+    displayRows: function() {
+      return (this.byRows = true);
+    }
   }
 });
