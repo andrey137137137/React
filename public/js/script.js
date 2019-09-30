227d77ab85ba359d8a995b80d469ea285a76a309
Bannikov Andrey <andrey27x777@gmail.com>
2019-09-30 04:01:18 +0300

    Edited shadow

--- public/js/script.js
+++ public/js/script.js
@@ -1,34 +1,27 @@
 var app = new Vue({
   el: "#app",
   data: {
-    byRows: true
+    byRows: false
   },
   computed: {
     containerClasses: function() {
       return {
         "pr-0": this.byRows,
-        "pb-37": !this.byRows,
-        "pl-27": !this.byRows,
-        "pt-md-4": !this.byRows,
-        "pr-md-19": !this.byRows,
-        "pb-md-14": !this.byRows,
-        "pl-md-23": !this.byRows,
+        // "pb-37": !this.byRows,
+        // "pl-27": !this.byRows,
+        // "pt-md-4": !this.byRows,
+        // "pr-md-19": !this.byRows,
+        // "pb-md-14": !this.byRows,
+        // "pl-md-23": !this.byRows,
         "shadow-sm": !this.byRows,
-        "section-container--light_bg": !this.byRows
+        "section-container--light_bg": !this.byRows,
+        "main-container--blocks": !this.byRows
       };
     },
     cardModifClass: function() {
       return {
         "card--row": this.byRows
       };
-    }
-  },
-  methods: {
-    displayBlocks: function() {
-      return (this.byRows = false);
-    },
-    displayRows: function() {
-      return (this.byRows = true);
     }
   }
 });
