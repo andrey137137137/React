2bc3fca683fee85b3b4b990ca9eedf5a399dd636
Bannikov Andrey <andrey27x777@gmail.com>
2019-09-30 04:44:00 +0300

    Refactored display card classes

--- public/js/script.js
+++ public/js/script.js
@@ -6,21 +6,10 @@ var app = new Vue({
   computed: {
     containerClasses: function() {
       return {
-        "pr-0": this.byRows,
-        // "pb-37": !this.byRows,
-        // "pl-27": !this.byRows,
-        // "pt-md-4": !this.byRows,
-        // "pr-md-19": !this.byRows,
-        // "pb-md-14": !this.byRows,
-        // "pl-md-23": !this.byRows,
+        "main-container--rows": this.byRows,
         "shadow-sm": !this.byRows,
         "section-container--light_bg": !this.byRows,
         "main-container--blocks": !this.byRows
-      };
-    },
-    cardModifClass: function() {
-      return {
-        "card--row": this.byRows
       };
     }
   }
