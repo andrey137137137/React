6954730669568849af620c5301133bd38bd50f0b
Bannikov Andrey <andrey27x777@gmail.com>
2020-05-19 03:55:54 +0300

    Prepared FilterForm component

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { observable, computed } from "mobx";
+import { observable, configure, computed, action } from "mobx";
 import { observer } from "mobx-react";
+// configure({ enforceActions: "observed" });
 
 // var app = new Vue({
 //   el: "#app",
@@ -109,28 +110,31 @@ class FilterBtn extends Component {
     "d-none d-md-inline-block pr-md-17 filter-text",
     "text-success",
   );
-  @computed get linkClassesComputed() {
+
+  @computed get linkComputedClasses() {
     return (
       this.linkClasses.common +
       " " +
       getSwitchedClass(this.showFilter, this.linkClasses.switched)
     );
   }
-  @computed get textClassesComputed() {
+  @computed get textComputedClasses() {
     return (
       this.textClasses.common +
       " " +
       getSwitchedClass(this.showFilter, this.textClasses.switched)
     );
   }
-  handle = () => {
+
+  @action handle = () => {
     this.showFilter = !this.showFilter;
   };
+
   render() {
     return (
-      <NavLink classes={this.linkClassesComputed} clickHandle={this.handle}>
+      <NavLink classes={this.linkComputedClasses} clickHandle={this.handle}>
         <div className="icon-bg"></div>
-        <span className={this.textClassesComputed}> Фильтр </span>
+        <span className={this.textComputedClasses}> Фильтр </span>
         <div className="icon-symbol_wrap">
           <SvgCmp
             icon="filter"
@@ -147,6 +151,7 @@ class SwitcherBtn extends Component {
   @observable byRows = this.props.byRows;
   @observable linkPostClasses = "filter-link--switcher";
   @observable iconClasses = getComputedClasses("icon", "icon--success");
+
   @computed get rowsClasses() {
     return (
       this.iconClasses.common +
@@ -161,12 +166,14 @@ class SwitcherBtn extends Component {
       getSwitchedClass(!this.byRows, this.iconClasses.switched)
     );
   }
-  rowsHandle = () => {
+
+  @action rowsHandle = () => {
     this.byRows = true;
   };
-  squarsHandle = () => {
+  @action squarsHandle = () => {
     this.byRows = false;
   };
+
   render() {
     return (
       <>
@@ -187,118 +194,157 @@ class SwitcherBtn extends Component {
   }
 }
 
-// @observer
-// class FilterForm extends Component {
-//   constructor(props) {
-//     super(props);
+@observer
+class FilterForm extends Component {
+  @observable showFilter = this.props.showFilter;
+  @observable activeTab = 2;
+  @observable formClasses = getComputedClasses(
+    "form section section--tabs_wrap filter_form mt-md-n1",
+    "d-none",
+  );
+  @observable tabItemClasses = getComputedClasses(
+    "nav-item filter_form-item",
+    "nav-item--active",
+  );
+  @observable tabLinkClasses = getComputedClasses(
+    "nav-link filter_form-link",
+    "nav-link--active",
+  );
+  @observable tabs = ["Все", "Сериалы", "Передачи"];
+  @observable channels = ["Первый", "ТНТ", "Россия 1"];
+  @observable channelSelect = [1, 2, 3, 4, 5];
+  @observable genres = ["Боевик", "Ужасы", "Фантастика", "Комедия"];
+  @observable filters = ["Все года...", "Все возраста...", "Все страны..."];
 
-//     const commonClasses =
-//       "form.section.section--tabs_wrap.filter_form.mt-md-n1";
-//     const switchedClass = "d-none";
-//     const successClasses = getSwitchedClass(props.showFilter, switchedClass);
+  @computed get formComputedClasses() {
+    return (
+      this.formClasses.common +
+      " " +
+      getSwitchedClass(this.showFilter, this.formClasses.switched)
+    );
+  }
+  @computed get tabItemComputedClasses() {
+    return (
+      this.tabItemClasses.common +
+      " " +
+      getSwitchedClass(this.activeTab, this.tabItemClasses.switched)
+    );
+  }
+  @computed get tabLinkComputedClasses() {
+    return (
+      this.tabLinkClasses.common +
+      " " +
+      getSwitchedClass(this.activeTab, this.tabLinkClasses.switched)
+    );
+  }
 
-//     const tabItemCommonClasses = ".nav-item.filter_form-item";
-//     const tabItemSwitchedClass = "nav-item--active";
+  @action getTabItemClass = (index) => {
+    return {
+      "nav-item--active": this.state.activeTab == index,
+    };
+  };
+  @action getTabLinkClass = (index) => {
+    return {
+      active: this.state.activeTab == index,
+    };
+  };
+  @action tabHandle = (index) => {
+    this.activeTab = index;
+  };
 
-//     const tabLinkCommonClasses = ".nav-link.filter_form-link";
-
-//     this.state = {
-//       commonClasses,
-//       switchedClass,
-//       successClasses,
-//       showFilter: props.showFilter,
-//       activeTab: 2,
-//     };
-//   }
-//   getTabItemClass(index) {
-//     return {
-//       "nav-item--active": this.state.activeTab == index,
-//     };
-//   }
-//   getTabLinkClass(index) {
-//     return {
-//       active: this.state.activeTab == index,
-//     };
-//   }
-//   render() {
-//     return (
-//       <form className={this.state.successClasses} action="">
-//         <div className=".row.section-header">
-//           <div className=".col-12.col-md-auto.navbar.section-top">
-//             <h3 className=".navbar-brand.section-top_title"> Фильтр </h3>
-//             <button className=".btn.btn-link.filter_form-reset" type="reset">
-//               Сбросить
-//             </button>
-//           </div>
-//           <ul className=".col.nav.nav-tabs.order-md-first.filter_form-tabs">
-//             <li
-//               className=".nav-item.filter_form-item"
-//               class="getTabItemClass(0)"
-//             >
-//               <a
-//                 className=".nav-link.filter_form-link"
-//                 href=""
-//                 class="getTabLinkClass(0)"
-//                 onClick={this.setState({ activeTab: 0 })}
-//               >
-//                 Все
-//               </a>
-//             </li>
-//           </ul>
-//         </div>
-//         <div className=".row.section-body.filter_form-row">
-//           <div className=".col-12">
-//             <div className=".row.no-gutters.justify-content-between">
-//               <div className=".col.col-md-auto">
-//                 <button
-//                   className=".btn.btn-outline-secondary.active.filter_form-channel"
-//                   type="button"
-//                 >
-//                   Все каналы
-//                 </button>
-//               </div>
-//               <div className=".col-auto.d-none.d-md-block">
-//                 <select className=".form-control.filter_form-channel_dropbox"></select>
-//               </div>
-//             </div>
-//           </div>
-//           <div className=".col-12.col-sm-6.col-md-3.filter_form-control_wrap">
-//             <div className=".filter_form-control.filter_form-control--active">
-//               <div className=".filter_form_category">
-//                 <div className=".filter_form_category-text"> Фантастика </div>
-//                 <a className=".icon.filter_form_category-close" href="">
-//                   <SvgCmp icon="close" classes="icon-symbol" />
-//                 </a>
-//               </div>
-//             </div>
-//           </div>
-//           <div className=".col-12.col-sm-6.col-md-3.filter_form-control_wrap">
-//             <input
-//               className=".form-control.filter_form-control"
-//               placeholder="Все года..."
-//             />
-//           </div>
-//           <div className=".col-12.mt-17">
-//             <div className=".row.mx-0">
-//               <button
-//                 className=".col-12.col-md-auto.btn.btn-secondary"
-//                 type="submit"
-//               >
-//                 Применить
-//               </button>
-//               <button
-//                 className=".d-none.col-auto.ml-10.btn.btn-secondary"
-//                 type="reset"
-//               >
-//                 Сбросить
-//               </button>
-//             </div>
-//           </div>
-//         </div>
-//       </form>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <form className={this.formComputedClasses} action="">
+        <div className="row section-header">
+          <div className="col-12 col-md-auto navbar section-top">
+            <h3 className="navbar-brand section-top_title"> Фильтр </h3>
+            <button className="btn btn-link filter_form-reset" type="reset">
+              Сбросить
+            </button>
+          </div>
+          <ul className="col nav nav-tabs order-md-first filter_form-tabs">
+            {this.tabs.map((value, index) => (
+              <li
+                key={index}
+                className={this.tabItemComputedClasses}
+                class={() => this.getTabItemClass(index)}
+              >
+                <a
+                  className={this.tabLinkComputedClasses}
+                  href=""
+                  class={() => this.getTabLinkClass(index)}
+                  onClick={() => this.tabHandle(index)}
+                >
+                  {value}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="row section-body filter_form-row">
+          <div className="col-12">
+            <div className="row no-gutters justify-content-between">
+              <div className="col col-md-auto">
+                <button
+                  className="btn btn-outline-secondary active filter_form-channel"
+                  type="button"
+                >
+                  Все каналы
+                </button>
+              </div>
+              <div className="col-auto d-none d-md-block">
+                <select className="form-control filter_form-channel_dropbox">
+                  {this.channelSelect.map((value, index) => (
+                    <option key={index}> {value} </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-3 filter_form-control_wrap">
+            <div className="filter_form-control filter_form-control--active">
+              {this.genres.map((value, index) => (
+                <div key={index} className="filter_form_category">
+                  <div className="filter_form_category-text"> {value} </div>
+                  <a className="icon filter_form_category-close" href="">
+                    <SvgCmp icon="close" classes="icon-symbol" />
+                  </a>
+                </div>
+              ))}
+            </div>
+          </div>
+          {this.filters.map((value, index) => (
+            <div
+              key={index}
+              className="col-12 col-sm-6 col-md-3 filter_form-control_wrap"
+            >
+              <input
+                className="form-control filter_form-control"
+                placeholder={value}
+              />
+            </div>
+          ))}
+          <div className="col-12 mt-17">
+            <div className="row mx-0">
+              <button
+                className="col-12 col-md-auto btn btn-secondary"
+                type="submit"
+              >
+                Применить
+              </button>
+              <button
+                className="d-none col-auto ml-10 btn btn-secondary"
+                type="reset"
+              >
+                Сбросить
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+  }
+}
 
 ReactDOM.render(
   <FilterBtn showFilter={false} />,
