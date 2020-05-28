76a7e1853aaeb191b7f921b97caed6ef41afef1f
Andrey Bannikov <andrey27x777@gmail.com>
2020-05-28 21:50:36 +0300

    Edited Pagination component

--- src/js/components/PaginationCmp.js
+++ src/js/components/PaginationCmp.js
@@ -1,28 +1,38 @@
 import React, { Component, Fragment } from "react";
-import { observable, action } from "mobx";
+import { observable, computed, action } from "mobx";
 import { observer } from "mobx-react";
 import SvgCmp from "@cmp/SvgCmp";
 import { getComputedClasses } from "@help/classes";
 
+const PageIconCmp = props => (
+  // span.icon.page-text.page-icon(class="icon-" + icon)
+  <SvgCmp
+    icon="angle"
+    classes={"icon icon-" + props.icon + " page-text page-icon"}
+  />
+);
+
+const PageTextCmp = props => (
+  <span className="d-none d-md-block page-text"> {props.value} </span>
+);
+
 export default
 @observer
 class PaginationCmp extends Component {
-  @observable len = 7;
+  @observable count = 7;
 
-  @action getIconElem(icon) {
-    return (
-      // span.icon.page-text.page-icon(class="icon-" + icon)
-      SvgCmp(
-        (icon = "angle"),
-        (className = "icon icon-" + icon + " page-text page-icon")
-      )
-    );
+  @computed get pages() {
+    const array = [];
+
+    for (let i = 0; i < this.count; i++) {
+      array.push(i + 1);
+    }
+
+    return array;
   }
-  @action getTextElem(value) {
-    return <span className="d-none d-md-block page-text"> {value} </span>;
-  }
+
   @action getItemClass(index) {
-    const hiddenCond = index == 4 || index == len - 2;
+    const hiddenCond = index == 4 || index == this.count - 2;
 
     return getComputedClasses([
       { cond: hiddenCond, value: "d-none" },
@@ -34,21 +44,20 @@ class PaginationCmp extends Component {
     return pug`
       Fragment
         ul.col-auto.pagination.mb-6.pr-19.pr-md-20
-          - for (var i = 0; i < len; i++)
-          
-            li.page-item(className=this.getItemClass(index))
-              //- a.page-link(href="" class={"ml-6": index == len - 1})
+          each item, index in this.pages
+            li.page-item(key=index className=this.getItemClass(index))
+              //- a.page-link(href="" class={"ml-6": index == this.count - 1})
               a.page-link(href="")
                 case index
                   when 0
-                    this.getIconElem("angle_left")
-                    this.getTextElem("Предыдущая")
-                  when len - 1
-                    this.getTextElem("Следующая")
-                    this.getIconElem("angle_right")
+                    PageIconCmp(icon="angle_left")
+                    PageTextCmp(value="Предыдущая")
+                  when this.count - 1
+                    PageTextCmp(value="Следующая")
+                    PageIconCmp(icon="angle_right")
                   default
                     | #{index}
-          
+
         //- .col-auto.pl-2.pr-0.page-status Страница 3 из 7
         .col-auto.pl-2.page-status Страница 3 из 7
     `;
